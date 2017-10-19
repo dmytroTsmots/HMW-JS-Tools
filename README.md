4c6bf67b12014efed94d3df5e86623dbb0acd7d4
dmytroTsmots <dmytro.tsmots@gmail.com>
2017-10-20 00:34:49 +0300

    update README.md with description and update gulpfile.js with commits

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # HMW-JS-Tools
-My JS-Tools HMW
+
+
+
+
+
+
+*Please, complite steps below*
+
+Install [Node.js](https://nodejs.org/uk/download/)
+
+Check **Node** version:
+>node -v
+
+Check **npm** version:
+>npm -v
+
+Install **gulp**:
+>npm install gulp -g
+
+Clone repo
+>git clone [https://github.com/dmytroTsmots/HMW-JS-Tools.git](https://github.com/dmytroTsmots/HMW-JS-Tools.git)
+
+Then type
+>npm install
+
+Get production release
+>npm run build
+
+Get development server release
+>npm run build:dev
+
+*Enjoy!*
--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,21 @@ var gulpSequence = require('gulp-sequence');
 var browserSync = require('browser-sync').create();
 
 
+//This task use jshint
 gulp.task('hint', function() {
     return gulp.src('./src/scripts/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-
+//This task only copy html file to prod folder
 gulp.task('copy', function () {
 	gulp.src('./src/*.html')
     .pipe(gulp.dest('./prod/'))
     .pipe(browserSync.stream());
 });
 
-
+//This task transpile es6 code to es5 with babel
 gulp.task('js', function () {
 	gulp.src('./src/scripts/ES6/*.js')
         .pipe(babel({
@@ -32,7 +33,7 @@ gulp.task('js', function () {
         .pipe(browserSync.stream());   
 });
 
-
+//This task compile .less files to .css files
 gulp.task('less', function () {
   return gulp.src('./src/**/*.less')
     .pipe(less())
@@ -40,7 +41,7 @@ gulp.task('less', function () {
     .pipe(browserSync.stream());
 });
 
-
+//This task concat and minify all .js files into app.js file
 gulp.task('optimize', function() {
   return gulp.src('./src/scripts/ES5/*.js')
     .pipe(concat('app.js'))
@@ -48,12 +49,12 @@ gulp.task('optimize', function() {
     .pipe(gulp.dest('./prod/js/'));
 });
 
-
+//This task delete prod folder
 gulp.task('del',function(){
     del.sync(['./prod/']);
 });
 
-
+//This task use browserSync to livereload browser page
 gulp.task('livereload', function() {
     browserSync.init({
         server: {
@@ -62,13 +63,15 @@ gulp.task('livereload', function() {
     });
     gulp.watch('src/css/*.less',['less']);
     gulp.watch("src/*.html",['copy']).on('change', browserSync.reload);
-    gulp.watch("src/js/**/*.js",['js', 'optimize']).on('change', browserSync.reload);
+    gulp.watch("src/scripts/**/*.js",['js', 'optimize']).on('change', browserSync.reload);
 });
 
 
-
+//Task prod for production release
 gulp.task('prod', gulpSequence(('del',['copy', 'less', 'js', 'optimize'])));
 
+//Task server for development server release (use watch)
 gulp.task('server', ['prod', 'livereload']);
 
-gulp.task('default', ['server']);
+//Default task
+gulp.task('default', ['prod']);
